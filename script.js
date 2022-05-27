41c6e75d2bd0612494300883ed6039f71d835cdb
Faith Oluwibe <faitholuwibe@gmail.com>
2022-05-27 10:56:39 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+// // const menu = document.querySelector('#mobile-menu')
+// // const navItem = document.querySelector('.nav_item')
+// //  const menuLinks = document.querySelector('.nav_link')
+// // const body = document.querySelector('body')
+// // // const cancel = document.querySelector('.cancel')
+
+// // const mobileMenu = () => {
+// // menu.classList.toggle('is-active')
+// // menuLinks.classList.toggle('active')
+// // body.classList.toggle('active')
+// // }
+
+// // menu.addEventListener('click', mobileMenu)
+
+
+// const mobileMenu = document.querySelector('#mobile-menu');
+// const navItem = document.querySelector('.nav_item')
+// const bar = document.querySelectorAll('.bar')
+// navItem.style.display="none";
+
+// const showSidebar = function() {
+//     navItem.style.display="grid";
+
+//     bar.forEach((line) => {
+//         line.classList.add(`.bar:nth-child(${line+1})`);
+//     })
+// }
+
+// mobileMenu.addEventListener('click', showSidebar);
+// console.log(navItem);
+// console.log(bar);
+
+//
